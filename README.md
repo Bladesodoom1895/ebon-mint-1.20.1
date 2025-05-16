6f6334506d33f3a5ed4c9c98914d84789f243a60
Dalas Neff <dalasneff@gmail.com>
2025-05-16 08:15:10 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ebon-mint-1.20.1
+Custom currency mod for Ebon Hollow
---
+++ src/main/java/net/blade/ebonmint/EbonMint.java
@@ -0,0 +1,16 @@
+package net.blade.ebonmint;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EbonMint implements ModInitializer {
+	public static final String MOD_ID = "ebonmint";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
